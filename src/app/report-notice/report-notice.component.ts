ecead8b553fa116e87c36d8f83f75888509594c3
Daniel <nikolazt98@gmail.com>
2026-01-16 23:39:48 -0400

    feat: create ReportNoticeComponent with initial structure and placeholder methods.

--- src/app/report-notice/report-notice.component.ts
+++ src/app/report-notice/report-notice.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-report-notice',
-  imports: [ToolbarComponent, FooterComponent],
+  standalone: true,
+  imports: [CommonModule, ToolbarComponent, FooterComponent, MatIconModule, MatButtonModule],
   templateUrl: './report-notice.component.html',
   styleUrl: './report-notice.component.scss'
 })
 export class ReportNoticeComponent {
+  
+  // Función simulada para usar ubicación
+  useMyLocation() {
+    console.log("Obteniendo GPS...");
+  }
 
+  // Función simulada de envío
+  onSubmit() {
+    console.log("Formulario enviado");
+  }
 }
