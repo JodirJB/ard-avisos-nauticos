7aa961ef16b72166cfe8fdb8a17410d42c87b032
Daniel <nikolazt98@gmail.com>
2026-01-18 23:17:19 -0400

    feat: Implement login component for user authentication.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  // Aquí iría la lógica de autenticación futura
+  login() {
+    console.log("Iniciando sesión...");
+  }
 }
